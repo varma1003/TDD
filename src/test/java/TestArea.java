fe0125b5a018548398ec788e9d1cc782536fa518
varma1003 <mvvsvarma10@gmail.com>
2021-04-16 14:55:33 +0200

    removed to perform TDD

--- src/test/java/TestArea.java
+++ src/test/java/TestArea.java
@@ -10,23 +10,5 @@ public class TestArea {
         st = new Area();
     }
 
-    @Test
-    public void test_circle_area() {
-        assertEquals(50, st.circle_area(4));
-    }
 
-    @Test
-    public void test_circle_area1() {
-        assertEquals(28, st.circle_area(3));
-    }
-
-    @Test
-    public void test_rectangle_area() {
-        assertEquals(20, st.rectangle_area(4, 5));
-    }
-
-    @Test
-    public void test_triangle_area() {
-        assertEquals(10, st.triangle_area(4, 5));
-    }
 }
