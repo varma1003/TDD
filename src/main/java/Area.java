f8bcae65cf59a4d44ef55ac869f96a40182cbab6
varma1003 <mvvsvarma10@gmail.com>
2021-04-18 00:30:24 +0200

    all test cases failed

--- src/main/java/Area.java
+++ src/main/java/Area.java
@@ -5,16 +5,19 @@ public class Area {
 
     public int circle_area(int r){
         double c_area = r * r * 3.14;
-        return (int) c_area;
+        //return (int) c_area;
+        return 0;
     }
 
     public int rectangle_area(int h, int l){
         double r_area = h * l;
-        return (int) r_area;
+        //return (int) r_area;
+        return 0;
     }
 
     public int triangle_area(int h, int b){
         double t_area = (h * b)/2.0;
-        return (int) t_area;
+        //return (int) t_area;
+        return 0;
     }
 }
