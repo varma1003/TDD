6f5bddce60f0aac69dd52f4a6b425403ccbb8aa4
varma1003 <mvvsvarma10@gmail.com>
2021-04-18 00:09:29 +0200

    code

--- src/main/java/Area.java
+++ src/main/java/Area.java
@@ -6,13 +6,11 @@ public class Area {
     public int circle_area(int r){
         double c_area = r * r * 3.14;
         return (int) c_area;
-        //return 0;
     }
 
     public int rectangle_area(int h, int l){
         double r_area = h * l;
         return (int) r_area;
-        //return 0;
     }
 
     public int triangle_area(int h, int b){
